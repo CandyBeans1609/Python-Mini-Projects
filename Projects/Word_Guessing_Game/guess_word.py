31c9ab30db1dd09533bb9a09eaa98bd586ffd38a
CandyBeans1609 <156273224+CandyBeans1609@users.noreply.github.com>
2024-06-29 22:59:16 +0530

    formatting and spelling mistakes fixed

--- Projects/Word_Guessing_Game/guess_word.py
+++ Projects/Word_Guessing_Game/guess_word.py
@@ -1,34 +1,38 @@
 import random
 
-name=input("Hieee Welcome to word guessing game!! \nEnter your name to begin: ")
-print("All the best ",name)
-words=['apple','blush','ring','python','coding','cute','valorant']
-word=random.choice(words)
+name = input("Hi, welcome to the word guessing game!\nEnter your name to begin: ")
+print("All the best, ", name)
+
+words = ['apple', 'blush', 'ring', 'python', 'coding', 'cute', 'valorant']
+word = random.choice(words)
 
 print("Guess the characters: ")
 
-guesses=""
-turns=15
-while turns>0:
-    failed=0
+guesses = ""
+turns = 15
+
+while turns > 0:
+    failed = 0
     for char in word:
-        if char in guesses:    
-            print(char,end=" ")
+        if char in guesses:
+            print(char, end=" ")
         else:
-            print("_")
-            failed+=1
-    if failed==0:
-        print("You win")
-        print("The word is: ",word)
+            print("_", end=" ")
+            failed += 1
+    print()  # Print new line after the word representation
     
-    print()
-    guess=input("Guess a character: ")
-    guesses+=guess
-
+    if failed == 0:
+        print("Congratulations, you win!")
+        print("The word is: ", word)
+        break  # Exit the game loop if the player wins
+    
+    guess = input("Guess a character: ").lower()  # Convert input to lowercase
+    guesses += guess
+    
     if guess not in word:
-        turns-=1
-        print("Wrog")
-        print("You have", +turns, "more guesses")
-
-        if turns==0:
-            print("You Loose :( Better Luck next time")
+        turns -= 1
+        print("Wrong guess.")
+        print("You have", turns, "more guesses left.")
+        
+        if turns == 0:
+            print("Sorry, you lose. Better luck next time.")
