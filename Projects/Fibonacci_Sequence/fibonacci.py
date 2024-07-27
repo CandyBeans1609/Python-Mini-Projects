4735e6d3880f539fb9c80e0d1d60732b4ab248ad
CandyBeans1609 <156273224+CandyBeans1609@users.noreply.github.com>
2024-07-27 14:14:05 +0530

    minor display chage

--- Projects/Fibonacci_Sequence/fibonacci.py
+++ Projects/Fibonacci_Sequence/fibonacci.py
@@ -1,9 +1,11 @@
 def fibo(n):
+    print("====The fibonacci sequence is: ====")
     a = 0
     b = 1
     for i in range(n):
         yield a
         a, b = b, a + b
+    
 
 n = 10  
 for num in fibo(n):
