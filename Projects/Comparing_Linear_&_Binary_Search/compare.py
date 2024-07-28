4cf19abe69d6dee616f9d8c4e3de4b0210b5f04c
CandyBeans1609 <156273224+CandyBeans1609@users.noreply.github.com>
2024-07-28 18:32:13 +0530

    logic

--- Projects/Comparing_Linear_&_Binary_Search/compare.py
+++ Projects/Comparing_Linear_&_Binary_Search/compare.py
@@ -4,43 +4,42 @@ import time
 def linear_search(lst, key):
     for i in range(len(lst)):
         if lst[i] == key:
-            return True
-    return False
+            return i
+    return -1
 
-def binary_search(lst, key, low, high):
-    if low > high:
-        return False
-    
+def binary_search(lst, key, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(lst) - 1
+
+    if high < low:
+        return -1
+
     mid = (low + high) // 2
     if lst[mid] == key:
         return mid
-    elif key > lst[mid]:
-        return binary_search(lst, key, mid + 1, high)
-    else:
+    elif key < lst[mid]:
         return binary_search(lst, key, low, mid - 1)
+    else:
+        return binary_search(lst, key, mid + 1, high)
 
-# Generate a list of random integers
-length = 100
-lst = random.sample(range(-200, 200), length)  
-sorted_list = sorted(lst)  
-print("Sorted List:", sorted_list)
+length = 10000
+sorted_list = set()
+while len(sorted_list) < length:
+    sorted_list.add(random.randint(-3 * length, 3 * length))
+sorted_list = sorted(list(sorted_list))
 
-key = 7
-if key not in sorted_list:
-    # If key is not in the list, add it for searching
-    sorted_list.append(key)
-    sorted_list = sorted(sorted_list)  # Re-sort the list after adding the key
-
-# Linear Search Timing
 print("\nLinear Search:")
 start = time.time()
-linear_search(sorted_list, key)
+for target in sorted_list:
+    linear_search(sorted_list, target)
 end = time.time()
-print(f"Search time for Linear Search: {(end - start):.6f} seconds")
+print("Linear search time: ", (end - start), "seconds")
 
-# Binary Search Timing
 print("\nBinary Search:")
 start = time.time()
-binary_search(sorted_list, key, low=0, high=len(sorted_list) - 1)
+for target in sorted_list:
+    binary_search(sorted_list, target)
 end = time.time()
-print(f"Search time for Binary Search: {(end - start):.6f} seconds")
+print("Binary search time: ", (end - start), "seconds")
