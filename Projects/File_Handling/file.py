c243c27b087d6e49e7cc47e7d835b54581429440
CandyBeans1609 <156273224+CandyBeans1609@users.noreply.github.com>
2024-07-29 19:59:00 +0530

    text file

---
+++ Projects/File_Handling/file.py
@@ -0,0 +1,7 @@
+def update():
+    roll=int(input("Enter the roll number: "))
+    name=input("Enter the name: ")
+    year=input("Enter the current year: ")
+    div=input("Enter the division: ")
+
+    with open(Projects\File_Handling\student_info.txt)
